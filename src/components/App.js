d58b3d5550520134a7cd3fbdfd20b70ba81ddacd
Vivek Kumar <singhvivek1999@yahoo.com>
2023-06-04 15:41:56 +0530

    refactor

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,13 @@
-import React, { useEffect, useMemo, useState } from 'react'
+import React, { useEffect, useMemo, useState } from 'react';
 import '../styles/App.css';
 import SearchBar from './SearchBar';
 import { MoviesList, MovieDetails } from './MoviesList&Detail';
 
 const App = () => {
   const [movies, setMovies] = useState([]);
-  const [searchTitle, setSearchTitle] = useState("");
-  const [sortBy, setSortBy] = useState("ascending");
-  const [selectedMovie, setSelectedMovie] = useState("");
+  const [searchTitle, setSearchTitle] = useState('');
+  const [sortBy, setSortBy] = useState('ascending');
+  const [selectedMovie, setSelectedMovie] = useState('');
 
   // Sorting Movies by Release Year Logic
   const sortedMovies = useMemo(() => {
@@ -15,29 +15,31 @@ const App = () => {
       return [];
     }
     return movies.sort((firstMovie, secondMovie) => {
-      const firstYear = firstMovie.release_date.split("-")[0];
-      const secondYear = secondMovie.release_date.split("-")[0];
+      const firstYear = firstMovie.release_date.split('-')[0];
+      const secondYear = secondMovie.release_date.split('-')[0];
       const first = parseInt(firstYear);
       const second = parseInt(secondYear);
-      if (sortBy === "descending") {
+      if (sortBy === 'descending') {
         return first - second;
       } else {
         return second - first;
       }
     });
-  }, [movies, sortBy])
+  }, [movies, sortBy]);
 
   return (
     <div id="main">
       <h1>Movie Search</h1>
       <SearchBar />
-      <button className="sort-btn">Sort Movies by release year ({sortBy})</button>
+      <button className="sort-btn">
+        Sort Movies by release year ({sortBy})
+      </button>
       <MoviesList />
-      <section className='movie-details'>
+      <section className="movie-details">
         <MovieDetails />
       </section>
     </div>
-  )
-}
+  );
+};
 
 export default App;
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,14 +1,16 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
-const SearchBar = (props) => {
-    const [searchInput, setSearchInput] = useState("");
-    
-    return (
-        <form>
-            <input type="text" className="search-input"/>
-            <button type="submit" className="search-btn">Search</button>
-        </form>
-    );
-}
+const SearchBar = props => {
+  const [searchInput, setSearchInput] = useState('');
+
+  return (
+    <form>
+      <input type="text" className="search-input" />
+      <button type="submit" className="search-btn">
+        Search
+      </button>
+    </form>
+  );
+};
 
 export default SearchBar;
